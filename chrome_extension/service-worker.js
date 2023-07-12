f214d91e554843e01805697c2642ea88f681bc59
brendanfreeman <bfreeman0@gmail.com>
2023-07-12 15:34:11 +0100

    placeholder changes with radio

--- chrome_extension/service-worker.js
+++ chrome_extension/service-worker.js
@@ -1,20 +1,34 @@
-console.log('THis is a popup!');
-
-const button = document.getElementById("submit")
-const search_box = document.getElementById("search_bar")
-const radio = document.getElementsByClassName("location")
-const gp_radio = document.getElementById("gp")
-const pharmacy_radio = document.getElementById("pharmacy")
-const gmc_search = "https://www.gmc-uk.org/doctors?text="
+const button = document.getElementById("submit");
+const search_box = document.getElementById("search_bar");
+const radio = document.getElementsByClassName("location");
+const gp_radio = document.getElementById("gp");
+const pharmacy_radio = document.getElementById("pharmacy");
+const gmc_search = "https://www.gmc-uk.org/doctors?text=";
+const england_radio = document.getElementById("gp_england");
+const wales_radio = document.getElementById("gp_wales");
+const scotland_radio = document.getElementById("gp_england");
 
 
 gp_radio.addEventListener("click", () => {
-    search_box.placeholder = "Enter Dr's name."
-})
+    search_box.placeholder = "Enter Dr's name.";
+});
 
-pharmacy_radio.addEventListener("click", () => {
-    search_box.placeholder = "Enter postcode/ town/ reg."
-})
+pharmacy_radio.addEventListener('click', () => {
+    search_box.placeholder = 'Enter postcode/town/reg.';
+}
+)
+
+england_radio.addEventListener("click", gp_postcode);
+
+wales_radio.addEventListener("click", gp_postcode);
+
+scotland_radio.addEventListener("click", gp_postcode);
+
+
+function gp_postcode() {
+    search_box.placeholder = "Enter GP's Postcode";
+};
+
 
 button.addEventListener("click", () => {
     for (i = 0; i < radio.length; i++) {
@@ -23,28 +37,24 @@ button.addEventListener("click", () => {
                 let location_type = radio[i].value
             }
         }
-    }
-    const search_input = String(document.getElementById("search_bar").value)
-    //console.log("button pressed " + location_type)
-    //console.log("search query: " + search_input)
+    };
+    const search_input = String(document.getElementById("search_bar").value);
 
     if (location_type == "pharmacy") {
-        console.log("length of location", search_input.length)
+        console.log("length of location", search_input.length);
         if (!isNaN(search_input)) {
-            const website = "https://www.pharmacyregulation.org/registers/pharmacy/registrationnumber/" + search_input
+            const website = "https://www.pharmacyregulation.org/registers/pharmacy/registrationnumber/" + search_input;
 
         } else {
-            const website = "https://www.pharmacyregulation.org/registers/pharmacy/nameortown/" + search_input
+            const website = "https://www.pharmacyregulation.org/registers/pharmacy/nameortown/" + search_input;
         }
-        console.log(website)
 
         console.log("pharmacy")
     } else {
-        const website = gmc_search + search_input
-        console.log("gp")
+        const website = gmc_search + search_input;
     }
     chrome.tabs.create({
         url: website
-    })
+    });
 
 });
