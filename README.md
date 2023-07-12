a65f7051b0a981126f9f685a2e3e7a902fc81301
brendanfreeman <bfreeman0@gmail.com>
2023-07-12 14:04:52 +0100

    replace var with const or let

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
+---
+
+Project Name: Entitlement to Receive Checker
+
+Current Version: (0.2.0)
+
+Description: Extension that aids checking if an address is entitled to
+pharmaceutical products.
+
+Creator: Brendan Freeman
+
+Creation Date: 25 April 2023
+
+Last Update By: Brendan Freeman
+
+Last Updated Date: 12 July 2023
+
+---
+
 # Entitlement To Receive Checker
-Description: A Chrome Extension that can be used to search the GPHC or the GMC register for needed data. Such as a doctor's registration details. Or a Pharmacy's registration details.
+
+Description: A Chrome Extension that can be used to search the GPHC or
+the GMC register for needed data. Such as a doctor's registration
+details. Or a Pharmacy's registration details.
 
 ## Main Features
-- Popup extension that gives the user option to search either GPHC or GMC.
+
+- Popup extension that gives the user option to search either GPHC or
+  GMC.
 - Loads either GPHC or GMC depending on option given.
--- chrome_extension/service-worker.js
+++ chrome_extension/service-worker.js
@@ -1,49 +1,50 @@
 console.log('THis is a popup!');
 
-var button = document.getElementById("submit")
-var search_box = document.getElementById("search_bar")
-var radio = document.getElementsByClassName("location")
-var gp_radio = document.getElementById("gp")
-var pharmacy_radio = document.getElementById("pharmacy")
-var gmc_search = "https://www.gmc-uk.org/doctors?text="
+const button = document.getElementById("submit")
+const search_box = document.getElementById("search_bar")
+const radio = document.getElementsByClassName("location")
+const gp_radio = document.getElementById("gp")
+const pharmacy_radio = document.getElementById("pharmacy")
+const gmc_search = "https://www.gmc-uk.org/doctors?text="
 
 
-gp_radio.addEventListener("click", ()=>{
+gp_radio.addEventListener("click", () => {
     search_box.placeholder = "Enter Dr's name."
 })
 
-pharmacy_radio.addEventListener("click", ()=>{
+pharmacy_radio.addEventListener("click", () => {
     search_box.placeholder = "Enter postcode/ town/ reg."
 })
 
-button.addEventListener("click", ()=>{
-    for(i = 0; i < radio.length; i++){
-        if(radio[i].type="radio"){
-            if(radio[i].checked){
-                var location_type = radio[i].value
+button.addEventListener("click", () => {
+    for (i = 0; i < radio.length; i++) {
+        if (radio[i].type = "radio") {
+            if (radio[i].checked) {
+                let location_type = radio[i].value
             }
         }
     }
-    var search_input = String(document.getElementById("search_bar").value)
+    const search_input = String(document.getElementById("search_bar").value)
     //console.log("button pressed " + location_type)
     //console.log("search query: " + search_input)
 
-    if(location_type == "pharmacy"){
+    if (location_type == "pharmacy") {
         console.log("length of location", search_input.length)
-        if(!isNaN(search_input)){
-            var website = "https://www.pharmacyregulation.org/registers/pharmacy/registrationnumber/" + search_input
+        if (!isNaN(search_input)) {
+            const website = "https://www.pharmacyregulation.org/registers/pharmacy/registrationnumber/" + search_input
 
-        } else{
-            var website = "https://www.pharmacyregulation.org/registers/pharmacy/nameortown/" + search_input 
+        } else {
+            const website = "https://www.pharmacyregulation.org/registers/pharmacy/nameortown/" + search_input
         }
         console.log(website)
 
         console.log("pharmacy")
-    } else{
-        var website = gmc_search + search_input
+    } else {
+        const website = gmc_search + search_input
         console.log("gp")
     }
     chrome.tabs.create({
-        url: website})
-    
+        url: website
+    })
+
 });
